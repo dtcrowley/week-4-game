0ba019ad7232d21665a715fb66f80b012e18a52c
dtcrowley <crowleyd1988@gmail.com>
2018-03-01 19:34:54 -0500

    Hoping I didn't just lose something...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,123 +1,91 @@
 $(document).ready(function(){
 
-    var targetNumber;
-    var userScore;
+    var targetNumber = Math.floor(Math.random() * 100 + 1);
+    
+    $("#target-number").text("Target Number: " + targetNumber); 
+
+    
+    var userScore = 0;
+    $("#user-score").text("Your Score: " + userScore);
+    
+    var wins = 0;
+    $("#wins").text("Wins: " + wins);
+    
+    var losses = 0;
+    $("#losses").text("Losses: " + losses)
+
     var green;
     var purple;
     var yellow;
     var blue;
-    var wins = 0;
-    var losses = 0;
-
+    
     var numberOptions = [2, 5, 6, 9];
 
-    for (var i = 0; i < numberOptions.length; i++) {
+    
+    function reset(){
+      var targetNumber = Math.floor(Math.random() * 100 + 1);
+      $("#target-number").text("Target Number: " + targetNumber); 
+      green= Math.floor(Math.random()*11+1);
+      purple= Math.floor(Math.random()*11+1);
+      yellow= Math.floor(Math.random()*11+1);
+      blue= Math.floor(Math.random()*11+1);
+      
+    } 
 
-      var image1 = $("<img>")
-      var image2 = $("<img>")
-      var image3 = $("<img>")
-      var image4 = $("<img>")
-  
-      image1.addClass("green");
-      image2.addClass("purple");
-      image3.addClass("yellow");
-      image4.addClass("blue");
-  
-      image1.attr("src", "assets/images/emerald.png")
-      image2.attr("src", "assets/images/purple.png")
-      image3.attr("src", "assets/images/yellow.png")
-      image4.attr("src", "assets/images/blue.png")
-
-      image1.attr("data-crystalvalue", numberOptions[i]);
-      image2.attr("data-crystalvalue", numberOptions[i]);
-      image3.attr("data-crystalvalue", numberOptions[i]);
-      image4.attr("data-crystalvalue", numberOptions[i]);
-
-
-    };
-
-    function start(){
-      targetNumber = Math.floor(Math.random() * 100) + 1;
-      userScore = 0;
-      $("#target-number").text("Target Number: " + targetNumber);
-      $("#user-score").text("Your Score: " + userScore);
-
-
+    function wins(){
+      alert("Hey, you won!");
+      wins++; 
+      $('#wins').text("Wins: " + wins);
+      reset();
     }
 
+    function losses(){
+      alert("Oops... You lost!");
+      losses++; 
+      $('#losses').text("Losses: " + losses);
+      reset();
+    }
 
+    $('#crystal1').on ('click', function(){
+      newUserScore = userScore + green;
+      $('#user-score').text(newUserScore); 
+      if (newUserScore == targetNumber){
+        wins();
+      }
+      else if ( newUserScore > targetNumber){
+        losses();
+      }
+    
+    
+    $('#crystal2').on ('click', function(){
+      newUserScore = userScore + purple;
+      $('#user-score').text(newUserScore); 
+      if (newUserScore == targetNumber){
+        wins();
+      }
+      else if ( newUserScore > targetNumber){
+        losses();
+      }   
+    
+    $('#crystal3').on ('click', function(){
+      newUserScore = userScore + yellow;
+      $('#user-score').text(newUserScore); 
+      if (newUserScore == targetNumber){
+        wins();
+        }
+      else if ( newUserScore > targetNumber){
+        losses();
+      }   
+    
 
-
-    start ();
-    $(".buttons").on(function(){
-
-    })
-
-
-
-
-
-
-})
-
-// ===========================================================================================================
-//var targetNumber = 53;
-
-//$("#number-to-guess").text(targetNumber);
-
-//var crystals = $("#crystals");
-
-//var counter = 0;
-
-// Now for the hard part. Creating multiple crystals each with their own unique number value.
-
-// We begin by expanding our array to include four options.
-//var numberOptions = [10, 5, 3, 7];
-
-// Next we create a for loop to create crystals for every numberOption.
-//for (var i = 0; i < numberOptions.length; i++) {
-
-  // For each iteration, we will create an imageCrystal
- // var imageCrystal = $("<img>");
-
-  // First each crystal will be given the class ".crystal-image".
-  // This will allow the CSS to take effect.
- // imageCrystal.addClass("crystal-image");
-
-  // Each imageCrystal will be given a src link to the crystal image
-  //imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-  // Each imageCrystal will be given a data attribute called data-crystalValue.
-  // This data attribute will be set equal to the array value.
-  //imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-  // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
- // crystals.append(imageCrystal);
-//}
-
-// This time, our click event applies to every single crystal on the page. Not just one.
-//crystals.on("click", ".crystal-image", function() {
-
-  // Determining the crystal's value requires us to extract the value from the data attribute.
-  // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-  // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-  // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
- // var crystalValue = ($(this).attr("data-crystalvalue"));
-  //crystalValue = parseInt(crystalValue);
-  // We then add the crystalValue to the user's "counter" which is a global variable.
-  // Every click, from every crystal adds to the global counter.
-  counter += crystalValue;
-
-  // All of the same game win-lose logic applies. So the rest remains unchanged.
-  //alert("New score: " + counter);
-
-  //if (counter === targetNumber) {
-    //alert("You win!");
- // }
-
- // else if (counter >= targetNumber) {
-   // alert("You lose!!");
-  ////}
-
-//}//);
+    $('#crystal4').on ('click', function(){
+      newUserScore = userScore + blue;
+      $('#user-score').text(newUserScore); 
+      if (newUserScore == targetNumber){
+        wins();
+      }
+      else if ( newUserScore > targetNumber){
+        losses();
+      }   
+    }
+    
